17ba7a2ce242ae9fdda653a55132b8e77a3d3ff4
David Lai <dlai@edgecast.com>
2015-12-29 13:40:31 -0800

    initial state

---
+++ addon/components/ripple-effect.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import layout from '../templates/components/ripple-effect';
+
+export default Ember.Component.extend({
+  layout: layout
+});
---
+++ app/components/ripple-effect.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-ripple-effect/components/ripple-effect';
